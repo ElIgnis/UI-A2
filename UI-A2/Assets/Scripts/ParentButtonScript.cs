d27e5d27b5e8098e808a0fc93d4fdcebfc80eea9
ElIgnis <Intervention.408@live.com>
2016-01-07 21:15:12 +0800

    test

--- UI-A2/Assets/Scripts/ParentButtonScript.cs
+++ UI-A2/Assets/Scripts/ParentButtonScript.cs
@@ -39,18 +39,37 @@ public class ParentButtonScript : MonoBehaviour
 
     private Vector3 initialPoint, finalPoint;
     private Vector3 finalPos;
-    private float initialTime, endTime, flickSpeed, RotationSpeed;
+    private float initialTime, flickSpeed, RotationSpeed, holdTimer;
+    private bool isFlicked;
+
+
 
     // Use this for initialization
     void Start()
     {
+        isFlicked = false;
         RotationSpeed = 400.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //FlickWheel();
+        //if (isFlicked == true)
+        //{
+           
+        //}
+        //else
+        //{
+        //    if (Input.GetMouseButton(0))
+        //    {
+        //        holdTimer += Time.deltaTime;
+        //        if(holdTimer > 0.2f)
+        //        {
+        //            SpinWheel();
+        //        }
+        //    }
+        //}
     }
     
     public void SpinWheel()
@@ -108,73 +127,76 @@ public class ParentButtonScript : MonoBehaviour
         return angle * (point - pivot) + pivot;
     }
 
-    public void FlickBegin()
-    {
-        //Store pos of flick
-        initialPoint = Input.mousePosition;
-        initialTime += Time.deltaTime;
-    }
+    //public void FlickBegin()
+    //{
+    //    //Store pos of flick
+    //    initialPoint = Input.mousePosition;
+    //    initialTime += Time.deltaTime;
+    //    isFlicked = true;
+    //}
 
-    public void FlickEnd()
-    {
-        if(initialTime != 0.0f)
-        {
-            finalPoint = Input.mousePosition;
-            flickSpeed = (finalPoint - initialPoint).magnitude / initialTime;
-            
+    //public void FlickWheel()
+    //{
+    //    if (Input.GetMouseButtonDown(0) == false)
+    //    {
+    //        if (initialTime > 0.0f)
+    //        {
+    //            finalPoint = Input.mousePosition;
+    //            flickSpeed = ((finalPoint - initialPoint).magnitude) * 5.0f;
+    //            initialTime = 0.0f;
+    //        }
+    //        Vector3 difference = finalPoint - initialPoint;
+    //        WheelAngle = difference.normalized.y * flickSpeed * Time.deltaTime;
 
-            Vector3 difference = finalPoint - initialPoint;
-
-            WheelAngle = difference.normalized.y * flickSpeed * Time.deltaTime;
-
-            //Calc the angle to spin for all buttons
-            for (int i = 0; i < NumAngles; ++i)
-            {
-                switch (i)
-                {
-                    case 0:
-                        finalPos = RotatePointAroundPoint(OptionBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, WheelAngle));
-                        OptionBtn.transform.position = finalPos;
-                        OptionBtnPos = finalPos;
-                        break;
-                    case 1:
-                        finalPos = RotatePointAroundPoint(CreditsBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, WheelAngle));
-                        CreditsBtn.transform.position = finalPos;
-                        CreditsBtnPos = finalPos;
-                        break;
-                    case 2:
-                        finalPos = RotatePointAroundPoint(HSBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, WheelAngle));
-                        HSBtn.transform.position = finalPos;
-                        HSBtnPos = finalPos;
-                        break;
-                    case 3:
-                        finalPos = RotatePointAroundPoint(InfoBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, WheelAngle));
-                        InfoBtn.transform.position = finalPos;
-                        InfoBtnPos = finalPos;
-                        break;
-                    case 4:
-                        finalPos = RotatePointAroundPoint(ShopBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, WheelAngle));
-                        ShopBtn.transform.position = finalPos;
-                        ShopBtnPos = finalPos;
-                        break;
-                    case 5:
-                        finalPos = RotatePointAroundPoint(FLBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, WheelAngle));
-                        FLBtn.transform.position = finalPos;
-                        FLBtnPos = finalPos;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        //Decrease speed overtime
-        if (flickSpeed > 0.0f)
-        {
-            flickSpeed -= Time.deltaTime;
-        }
-        else
-        {
-            initialTime = 0.0f;
-        }
-    }
+    //        //Calc the angle to spin for all buttons
+    //        for (int i = 0; i < NumAngles; ++i)
+    //        {
+    //            switch (i)
+    //            {
+    //                case 0:
+    //                    finalPos = RotatePointAroundPoint(OptionBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, -WheelAngle));
+    //                    OptionBtn.transform.position = finalPos;
+    //                    OptionBtnPos = finalPos;
+    //                    break;
+    //                case 1:
+    //                    finalPos = RotatePointAroundPoint(CreditsBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, -WheelAngle));
+    //                    CreditsBtn.transform.position = finalPos;
+    //                    CreditsBtnPos = finalPos;
+    //                    break;
+    //                case 2:
+    //                    finalPos = RotatePointAroundPoint(HSBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, -WheelAngle));
+    //                    HSBtn.transform.position = finalPos;
+    //                    HSBtnPos = finalPos;
+    //                    break;
+    //                case 3:
+    //                    finalPos = RotatePointAroundPoint(InfoBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, -WheelAngle));
+    //                    InfoBtn.transform.position = finalPos;
+    //                    InfoBtnPos = finalPos;
+    //                    break;
+    //                case 4:
+    //                    finalPos = RotatePointAroundPoint(ShopBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, -WheelAngle));
+    //                    ShopBtn.transform.position = finalPos;
+    //                    ShopBtnPos = finalPos;
+    //                    break;
+    //                case 5:
+    //                    finalPos = RotatePointAroundPoint(FLBtn.transform.position, Wheel.rectTransform.position, Quaternion.Euler(0, 0, -WheelAngle));
+    //                    FLBtn.transform.position = finalPos;
+    //                    FLBtnPos = finalPos;
+    //                    break;
+    //                default:
+    //                    break;
+    //            }
+    //        }
+    //        //Decrease speed overtime
+    //        if (flickSpeed - Time.deltaTime < 0.0f && isFlicked == true)
+    //        {
+    //            flickSpeed = 0.0f;
+    //            isFlicked = false;
+    //        }
+    //        else
+    //        {
+    //            flickSpeed -= 100.0f * Time.deltaTime;
+    //        }
+    //    }
+    //}
 }
