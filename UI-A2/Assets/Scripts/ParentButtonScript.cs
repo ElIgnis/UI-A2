2577419b24d7b0f0f2174a228e7f4fe447da67f8
ElIgnis <Intervention.408@live.com>
2016-01-07 13:00:48 +0800

    Added test wheel

---
+++ UI-A2/Assets/Scripts/ParentButtonScript.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ParentButtonScript : MonoBehaviour
+{
+    public Button PlayBtn;
+    public Button CreditsBtn;
+    public Button HSBtn;
+    public Button InfoBtn;
+    public Button ShopBtn;
+    public Button FLBtn;
+    public Button OptionBtn;
+
+    public static Vector3 PlayBtnPos;
+    public static bool PlayActive;
+
+    public static Vector3 CreditsBtnPos;
+    public static bool CreditsActive;
+
+    public static Vector3 HSBtnPos;
+    public static bool HSActive;
+
+    public static Vector3 InfoBtnPos;
+    public static bool InfoActive;
+
+    public static Vector3 ShopBtnPos;
+    public static bool ShopActive;
+
+    public static Vector3 FLBtnPos;
+    public static bool FLActive;
+
+    public static Vector3 OptionBtnPos;
+    public static bool OptionActive;
+
+    public Image Wheel;
+    private float NumAngles;
+    private float WheelAngle;
+    static public float[] AngleList = new float[6];
+    Vector3 initialPoint, finalPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        NumAngles = 6;
+        for(int i = 1; i < NumAngles; ++i)
+        {
+            AngleList[i] = AngleList[0] + i * 60;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MouseDown()
+    {
+        initialPoint = Input.mousePosition;
+    }
+    
+    public void SpinWheel()
+    {
+        Vector3 newPos;
+        Vector3 finalPos;
+        float WheelArc = Wheel.rectTransform.rect.width / 2;
+
+        //Drag direction check
+        finalPoint = Input.mousePosition;
+        Vector3 difference = finalPoint - initialPoint;
+        
+        WheelAngle += difference.y * 0.001f;
+
+        //Calc the angle to spin for all buttons
+        for (int i = 0; i < NumAngles; ++i)
+        {
+            AngleList[i] += WheelAngle;
+            newPos = new Vector3(Wheel.rectTransform.position.x * Mathf.Sin(AngleList[i]), Wheel.rectTransform.position.y * Mathf.Cos(AngleList[i]), 0.0f);
+            finalPos = Wheel.rectTransform.position + (newPos).normalized * WheelArc;
+
+            switch (i)
+            {
+                case 0:
+                    PlayBtn.transform.position = finalPos;
+                    PlayBtnPos = finalPos;
+                    break;
+                case 1:
+                    CreditsBtn.transform.position = finalPos;
+                    CreditsBtnPos = finalPos;
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+        
+        
+        //newPos = Wheel.rectTransform.position + (Wheel.rectTransform.position).normalized * WheelArc;
+        
+        
+        
+        //TODO: Wheel rotation
+        //if(buttonType == PlayBtn)
+        //{
+        //    buttonType.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f);
+        //    PlayBtnPos = buttonType.transform.position;
+        //}
+        //if (buttonType == CreditsBtn)
+        //{
+        //    buttonType.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f);
+        //    CreditsBtnPos = buttonType.transform.position;
+        //}
+    }
+}
--- UI-A2/Assets/Scripts/SceneChanger.cs
+++ UI-A2/Assets/Scripts/SceneChanger.cs
@@ -1,19 +1,82 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SceneChanger : MonoBehaviour {
-    
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class SceneChanger : MonoBehaviour
+{
 
-    public void ChangeToMenu() {
-        Application.LoadLevel("MainMenu");
+    // Use this for initialization
+    void Start()
+    {
+
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeToMenu()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Application.LoadLevel("MainMenu");
+        }
+    }
+
+    //public void ChangeToLvlSelect()
+    //{
+    //    if (Input.GetMouseButtonUp(0))
+    //    {
+    //        Application.LoadLevel("LevelSelect");
+    //    }
+    //}
+
+    public void ChangeToCredits()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            Application.LoadLevel("Credits");
+        }
+    }
+
+    //public void ChangeToHS()
+    //{
+    //    if (Input.GetMouseButtonUp(0))
+    //    {
+    //        Application.LoadLevel("Highscore");
+    //    }
+    //}
+
+    //public void ChangeToInfo()
+    //{
+    //    if (Input.GetMouseButtonUp(0))
+    //    {
+    //        Application.LoadLevel("InfoScreen");
+    //    }
+    //}
+
+    //public void ChangeToShop()
+    //{
+    //    if (Input.GetMouseButtonUp(0))
+    //    {
+    //        Application.LoadLevel("Shop");
+    //    }
+    //}
+
+    //public void ChangeToFL()
+    //{
+    //    if (Input.GetMouseButtonUp(0))
+    //    {
+    //        Application.LoadLevel("FriendList");
+    //    }
+    //}
+
+    //public void ChangeToOption()
+    //{
+    //    if (Input.GetMouseButtonUp(0))
+    //    {
+    //        Application.LoadLevel("Option");
+    //    }
+    //}
 }
