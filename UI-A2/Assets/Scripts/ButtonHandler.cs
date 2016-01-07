ba3676b23f419f2813c9579ff0cc9079804729da
Princeton Chew <PrincetonCYK@hotmail.com>
2016-01-07 18:55:31 +0900

    Shop avatar almost there.

---
+++ UI-A2/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public Button VIPButton;
+    public Button AvatarButton;
+    public Button PowerUpButton;
+    public Button BackButton;
+    public Button GemButton;
+
+    public Button SaberOptionButton;
+    public Button RinOptionButton;
+    public Button ShikiOptionButton;
+
+    public Image SaberPopUp;
+    public Image RinPopUp;
+    public Image ShikiPopUp;
+
+    public Button PopUpBackButton;
+    public Button PopUpBuyButton;
+
+    //Non Pop up buttons
+    private bool VIPButton_Active,
+                AvatarButton_Active,
+                PowerUpButton_Active,
+                GemButton_Active,
+                SaberOptionButton_Active,
+                RinOptionButton_Active,
+                ShikiOptionButton_Active;
+    //Pop up buttons
+    private bool PopUpBack_Active,
+                 PopUpBuy_Active;
+    // Use this for initialization
+    void Start()
+    {
+        SaberPopUp.gameObject.SetActive(false);
+        RinPopUp.gameObject.SetActive(false);
+        ShikiPopUp.gameObject.SetActive(false);
+
+        VIPButton_Active = true;
+        AvatarButton_Active = true;
+        PowerUpButton_Active = true;
+        GemButton_Active = true;
+        SaberOptionButton_Active = true;
+        RinOptionButton_Active = true;
+        ShikiOptionButton_Active = true;
+
+        PopUpBuyButton.gameObject.SetActive(false);
+        PopUpBackButton.gameObject.SetActive(false);
+
+        PopUpBack_Active = false;
+        PopUpBuy_Active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SaberOptionSelected()
+    {
+        if (SaberOptionButton_Active)
+        {
+            SaberPopUp.gameObject.SetActive(true);
+            SetNonPopUpButtonsActive(false);
+            SetPopUpButtonsActive(true);
+        }
+    }
+    public void RinOptionSelected()
+    {
+        if (RinOptionButton_Active)
+        {
+            RinPopUp.gameObject.SetActive(true);
+            SetNonPopUpButtonsActive(false);
+            SetPopUpButtonsActive(true);
+        }
+    }
+    public void ShikiOptionSelected()
+    {
+        if (ShikiOptionButton_Active)
+        {
+            ShikiPopUp.gameObject.SetActive(true);
+            SetNonPopUpButtonsActive(false);
+            SetPopUpButtonsActive(true);
+        }
+    }
+
+    public void PopUpBackSelected()
+    {
+        if (ShikiPopUp.gameObject.activeSelf)
+        {
+            ShikiPopUp.gameObject.SetActive(false);
+            SetNonPopUpButtonsActive(true);
+            SetPopUpButtonsActive(false);
+        }
+        else if (SaberPopUp.gameObject.activeSelf)
+        {
+            SaberPopUp.gameObject.SetActive(false);
+            SetNonPopUpButtonsActive(true);
+            SetPopUpButtonsActive(false);
+        }
+        else if (RinPopUp.gameObject.activeSelf)
+        {
+            RinPopUp.gameObject.SetActive(false);
+            SetNonPopUpButtonsActive(true);
+            SetPopUpButtonsActive(false);
+        }
+    }
+
+    public void PopUpBuySelected()
+    {
+
+    }
+
+    void SetNonPopUpButtonsActive(bool active)
+    {
+        VIPButton_Active = active;
+        AvatarButton_Active = active;
+        PowerUpButton_Active = active;
+        GemButton_Active = active;
+        SaberOptionButton_Active = active;
+        RinOptionButton_Active = active;
+        ShikiOptionButton_Active = active;
+    }
+
+    void SetPopUpButtonsActive(bool active)
+    {
+        PopUpBuy_Active = active;
+        PopUpBuyButton.gameObject.SetActive(active);
+
+        PopUpBack_Active = active;
+        PopUpBackButton.gameObject.SetActive(active);
+    }
+}
--- UI-A2/Assets/Scripts/TransitionHandler.cs
+++ UI-A2/Assets/Scripts/TransitionHandler.cs
@@ -6,30 +6,34 @@ public class TransitionHandler : MonoBehaviour
 {
     public Image InfoBarBG;
     public Image Title;
+    public Image DestinationPoint;
+    public ScrollRect ScrollingView;
 
     private Vector3 InfoBarBGPos;    //Current position
     private Vector3 TitlePos;   //Current position
- 
+    private Vector3 ScrollViewPos;   //Current position
+
     float Destination;  //Destination position
 
     public float TransitionSpeed = 10.0f;   //Speed of Transition
 
     public Vector3 worldPos;
 
+
     // Use this for initialization
     void Start()
     {
-        Destination = 315.25f;
+        Destination = DestinationPoint.transform.position.x;
         InfoBarBGPos = InfoBarBG.transform.position;
         TitlePos = Title.transform.position;
-        //InfoBarBG.transform.position = new Vector3(315.25f, InfoBarBGPos.y, 0.0f);
+        ScrollViewPos = ScrollingView.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         InfoBarTransition();
-
+        ScrollViewTransition();
         TitleTransition();
 
         worldPos = InfoBarBG.transform.position;
@@ -40,12 +44,11 @@ public class TransitionHandler : MonoBehaviour
         if (InfoBarBG.transform.position.x != Destination)
         {
             InfoBarBGPos.x += TransitionSpeed * Time.deltaTime;
-            if (InfoBarBGPos.x > Destination)
+            InfoBarBG.transform.Translate(TransitionSpeed * Time.deltaTime, 0, 0);
+            if (InfoBarBG.transform.position.x > Destination)
             {
-                InfoBarBGPos.x = Destination;
+                InfoBarBG.transform.position = new Vector3(Destination, InfoBarBG.transform.position.y, InfoBarBG.transform.position.z);
             }
-
-            InfoBarBG.transform.position = InfoBarBGPos;
         }
     }
 
@@ -54,12 +57,24 @@ public class TransitionHandler : MonoBehaviour
         if (Title.transform.position.x != Destination)
         {
             TitlePos.x += TransitionSpeed * Time.deltaTime;
-            if (TitlePos.x > Destination)
+            Title.transform.Translate(TransitionSpeed * Time.deltaTime, 0, 0);
+            if (Title.transform.position.x > Destination)
             {
-                TitlePos.x = Destination;
+                Title.transform.position = new Vector3(Destination, Title.transform.position.y, Title.transform.position.z);
             }
+        }
+    }
 
-            Title.transform.position = TitlePos;
+    void ScrollViewTransition()
+    {
+        if (ScrollingView.transform.position.x != Destination)
+        {
+            ScrollViewPos.x += TransitionSpeed * Time.deltaTime;
+            ScrollingView.transform.Translate(TransitionSpeed * Time.deltaTime, 0, 0);
+            if (ScrollingView.transform.position.x > Destination)
+            {
+                ScrollingView.transform.position = new Vector3(Destination, ScrollingView.transform.position.y, ScrollingView.transform.position.z);
+            }
         }
     }
 }
