7227271a1b697833716557ea98692d12f04508d3
ElIgnis <Intervention.408@live.com>
2016-01-12 17:00:35 +0800

    Test Audio

---
+++ UI-A2/Assets/Scripts/ButtonAudio.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class ButtonAudio : MonoBehaviour
+{
+    public AudioClip SoundToPlay;
+    AudioSource audio;
+    private bool playSound;
+    // Use this for initialization
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            playSound = true;
+        }
+        else
+        {
+            playSound = false;
+        }
+
+        //if(playSound)
+        {
+            if (!audio.isPlaying)
+            {
+                //audio.PlayOneShot(SoundToPlay, 10);
+            }
+        }
+    }
+}
