f989b57e385f05c88edb2d169792ff82a7326fd1
Malcolm <blaze-trinity@hotmail.sg>
2016-01-24 03:26:02 +0800

    finished everything

---
+++ UI-A2/Assets/Scripts/FriendListButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FriendListButton : MonoBehaviour {
+
+	public GameObject ConfirmationUI;
+	bool duelButtonPressed;
+	// Use this for initialization
+	void Start () {
+		ConfirmationUI.SetActive (false);
+		duelButtonPressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void DuelPressed(){
+		if (!duelButtonPressed) {
+			ConfirmationUI.SetActive (true);
+			duelButtonPressed = true;
+		}
+	}
+
+	public void ConfirmationPressed(){
+		ConfirmationUI.SetActive (false);
+	}
+}
